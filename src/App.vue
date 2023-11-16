40683976d86f00a7ed6a9da9dcd009ae3bfc71e4
mauribanez <117604787+mauribanez@users.noreply.github.com>
2023-11-16 12:01:39 -0400

    commit fix errors

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,46 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="App">
+    <Navbar @open-sidebar="openSidebar" />
+    <Sidebar v-if="sidebarOpen" @close-sidebar="closeSidebar" />
+    <SearchContainer />
+    <Carousel />
+    <main>
+      <CocktailCard title="Negroni" description="Ginebra, vermouth rojo y hielo" />
+      <CocktailCard title="Chuflay" description="Whisky, lemon juice, maple syrup y hielo" />
+      <!-- ... más tarjetas -->
+    </main>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Navbar from './components/Navbar.vue'
+import Sidebar from './components/Sidebar.vue'
+import SearchContainer from './components/SearchContainer.vue'
+import Carousel from './components/Carousel.vue'
+import CocktailCard from './components/CocktailCard.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Navbar,
+    Sidebar,
+    SearchContainer,
+    Carousel,
+    CocktailCard
+  },
+  data() {
+    return {
+      sidebarOpen: false
+    }
+  },
+  methods: {
+    openSidebar() {
+      this.sidebarOpen = true;
+    },
+    closeSidebar() {
+      this.sidebarOpen = false;
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
